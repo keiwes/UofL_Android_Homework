f9358981a19abc37cd0f24de73df16af9ef91408
Keith Weston <keith@keiwes.com>
2013-10-21 11:59:41 -0400

    Adding everything to Homework 5 folder

--- homework5/GeoQuiz/src/com/keiwes/com/QuizActivity.java
+++ homework5/GeoQuiz/src/com/keiwes/com/QuizActivity.java
@@ -1,7 +1,10 @@
 package com.keiwes.com;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -56,12 +59,18 @@ public class QuizActivity extends Activity {
 		Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
 	}
 
+	@TargetApi(11)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.d(TAG, "onCreate(Bundle) called");
 		setContentView(R.layout.activity_quiz);
-
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			ActionBar actionBar = getActionBar();
+			actionBar.setSubtitle("Bodies of Water");
+		}
+		
 		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
 		mTrueButton = (Button) findViewById(R.id.true_button);
